8be3708c7ee10a574c4f918de7435cea7647c6a7
MattZub13 <mateozubieta13@gmail.com>
2023-04-27 22:20:13 -0400

    creacion del footer y el header para las vistas del proyecto

---
+++ view/footer.php
@@ -0,0 +1,35 @@
+<!-- Footer -->
+<footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        © 2022 Zoogler by Mannatthemes.
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End Footer -->
+
+
+        <!-- jQuery  -->
+        <script src="../public/horizontal/assets/js/jquery.min.js"></script>
+        <script src="../public/horizontal/assets/js/popper.min.js"></script>
+        <script src="../public/horizontal/assets/js/bootstrap.min.js"></script>
+        <script src="../public/horizontal/assets/js/modernizr.min.js"></script>
+        <script src="../public/horizontal/assets/js/waves.js"></script>
+        <script src="../public/horizontal/assets/js/jquery.slimscroll.js"></script>
+        <script src="../public/horizontal/assets/js/jquery.nicescroll.js"></script>
+        <script src="../public/horizontal/assets/js/jquery.scrollTo.min.js"></script>
+
+        <!-- KNOB JS -->
+        <script src="../public/horizontal/assets/plugins/jquery-knob/excanvas.js"></script>
+        <script src="../public/horizontal/assets/plugins/jquery-knob/jquery.knob.js"></script> 
+
+        <script src="../public/horizontal/assets/plugins/chart.js/chart.min.js"></script>
+        <script src="../public/horizontal/assets/pages/dashboard.js"></script>
+
+        <!-- App js -->
+        <script src="../public/horizontal/assets/js/app.js"></script>
+       
+    </body>
+</html>
