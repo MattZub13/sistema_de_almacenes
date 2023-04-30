332a624a3da34b5936893dbd1fd67ac6f26aaf31
Coshio12 <118566621+Coshio12@users.noreply.github.com>
2023-04-30 17:51:58 -0400

    Add files via upload

--- model/Categoria.php
+++ model/Categoria.php
@@ -18,22 +18,33 @@ Class Categoria{
 
     public function insertar($nombre)
 	{
-		$sql="INSERT INTO categoria(nombre_categoria)VALUES('$nombre');";
-		return ejecutarConsulta($sql);
+		$validacion=$this->comprueba_duplicados($nombre,0);
+		if($validacion==0){
+			$sql="INSERT INTO categoria(nombre_categoria)VALUES('$nombre');";
+			return ejecutarConsulta($sql);
+		}else{
+			return 0;
+		}
+		
 	}
 	
 
-    public function editar($id_categoria,$nombre,$estado)
+    public function editar($id_categoria,$nombre)
     {
     
-		$sql="UPDATE categoria SET nombre_categoria='$nombre', estado_categoria='$estado' WHERE id_categoria='$id_categoria';";
-        return ejecutarConsulta($sql);
+		$validacion=$this->comprueba_duplicados($nombre,$id_categoria);
+		if($validacion==0){
+			$sql="UPDATE categoria SET nombre_categoria='$nombre' 
+			WHERE id_categoria='$id_categoria';";
+			return ejecutarConsulta($sql);
+		}
+		else{return 0;}
 		
     }
 
     public function desactivar($id_categoria)
 	{
-		$sql="UPDATE categoria SET estado='0' WHERE id_categoria='$id_categoria'";
+		$sql="UPDATE categoria SET estado_categoria='0' WHERE id_categoria='$id_categoria'";
 		return ejecutarConsulta($sql);
 	}
 
@@ -44,10 +55,24 @@ Class Categoria{
 		return ejecutarConsulta($sql);
 	}
 
+	public function mostrar($id_categoria)
+	{
+		$sql="SELECT id_categoria, nombre_categoria, estado_categoria FROM categoria WHERE id_categoria='$id_categoria'";
+		return ejecutarConsultaSimpleFila($sql);
+	}
+
 	public function select()
 	{
 		$sql="SELECT id_categoria, nombre_categoria FROM categoria 
 		WHERE (estado_categoria=1) ORDER BY nombre_categoria ASC";
 		return ejecutarConsulta($sql);		
+	}
+
+	public function comprueba_duplicados($codigo,$id)
+	{
+		$resultado=0;
+		$sql="SELECT COUNT(id_categoria) AS id_categoria FROM categoria WHERE (nombre_categoria='$codigo') AND (id_categoria<>$id)";
+		$resultado = ejecutarConsultaSimpleFila($sql);
+		return $resultado['id_categoria'];		
 	}
 }
