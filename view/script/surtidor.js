5b4fe24874f889433317b131765665f18a7666df
Coshio12 <118566621+Coshio12@users.noreply.github.com>
2023-05-01 19:29:30 -0400

    Add files via upload

---
+++ view/script/surtidor.js
@@ -0,0 +1,178 @@
+var tabla;
+
+function init(){
+    //Para validación
+	listar();
+    $("#formulario").on("submit",function(e){
+		guardaryeditar(e);	
+	});
+    
+}
+
+//Función limpiar
+function limpiar()
+{
+    $("#id_surtidor").val("");
+    $("#ubicacion_surtidor").val("");
+    $("#telefono_surtidor").val("");
+}
+
+function listar(){
+    tabla=$('#tableLista').DataTable(
+        {
+            "lengthMenu": [ 10, 25, 50, 75, 100 ],//mostramos el menú de registros a revisar
+            "Processing": true,//Activamos el procesamiento del datatables
+            "ServerSide": true,//Paginación y filtrado realizados por el servidor
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+            },
+            dom: 'Bfrtip',//Definimos los elementos del control de tabla
+            buttons: [		          
+                        'copyHtml5',
+                        'excelHtml5',
+                        'csvHtml5',
+                        'pdf'
+                    ],
+            "ajax":
+                    {
+                        
+                        url: '../ajax/surtidor.php?op=0',
+                        type : "get",
+                        dataType : "json",						
+                        error: function(e){
+                            console.log(e.responseText);	
+                        }
+                    },
+            "Destroy": true,
+            "iDisplayLength": 10,//Paginación
+            "order": [[ 0, "desc" ]]//Ordenar (columna,orden)
+        }
+    );
+}
+
+function guardaryeditar(e)
+{
+    
+	e.preventDefault(); //No se activará la acción predeterminada del evento
+	$("#btnGuardar").prop("disabled",true);
+	var formData = new FormData($("#formulario")[0]);
+	$.ajax({
+		url: "../ajax/surtidor.php?op=1",
+	    type: "POST",
+	    data: formData,
+	    contentType: false,
+	    processData: false,
+
+	    success: function(datos)
+	    {    
+            mensaje=datos.split(":");
+			if(mensaje[0]=="1"){               
+			swal.fire(
+				'Mensaje de Confirmación',
+				mensaje[1],
+				'success'
+
+				);           
+	          tabla.ajax.reload();
+			}
+			else{
+				Swal.fire({
+					type: 'error',
+					title: 'Error',
+					text: mensaje[1],
+					footer: 'Verifique la información de Registro, en especial que la información no fué ingresada previamente a la Base de Datos.'
+				});
+				console.log(datos);
+			}
+	    }
+
+	});
+	limpiar();
+}
+
+
+function mostrar(id_surtidor)
+{
+	$.post("../ajax/surtidor.php?op=4",{id_surtidor : id_surtidor}, function(data, status)
+	{
+		data = JSON.parse(data);
+		console.log(data);
+        $("#id_surtidor").val(data.id_surtidor);
+		$("#ubicacion").val(data.ubicacion_surtidor);
+        $("#telefono").val(data.telefono_surtidor);
+
+ 	});
+}
+
+function desactivar(id_surtidor)
+{
+	swal.fire({
+		title: 'Mensaje de Confirmación',
+		text: "¿Desea desactivar el Registro?",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		cancelButtonText: 'Cancelar',
+		confirmButtonText: 'Desactivar'
+	}).then((result) => {
+		if (result.value) {
+			$.post("../ajax/surtidor.php?op=2", {id_surtidor : id_surtidor}, function(e){
+				mensaje=e.split(":");
+					if(mensaje[0]=="1"){  
+						swal.fire(
+							'Mensaje de Confirmación',
+							mensaje[1],
+							'success'
+						);  
+						tabla.ajax.reload();
+					}	
+					else{
+						Swal.fire({
+							type: 'error',
+							title: 'Error',
+							text: mensaje[1],
+							footer: 'Verifique la información de Registro, en especial que la información no fué ingresada previamente a la Base de Datos.'
+						});
+					}			
+        	});	
+		}
+	});   
+}
+
+function activar(id_surtidor)
+{
+	swal.fire({
+		title: 'Mensaje de Confirmación',
+		text: "¿Desea activar el Registro?",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		cancelButtonText: 'Cancelar',
+		confirmButtonText: 'Activar'
+	}).then((result) => {
+		if (result.value) {
+			$.post("../ajax/surtidor.php?op=3", {id_surtidor : id_surtidor}, function(e){
+				mensaje=e.split(":");
+					if(mensaje[0]=="1"){  
+						swal.fire(
+							'Mensaje de Confirmación',
+							mensaje[1],
+							'success'
+						);  
+						tabla.ajax.reload();
+					}	
+					else{
+						Swal.fire({
+							type: 'error',
+							title: 'Error',
+							text: mensaje[1],
+							footer: 'Verifique la información de Registro, en especial que la información no fué ingresada previamente a la Base de Datos.'
+						});
+					}			
+        	});	
+		}
+	}); 
+}
+init();
