33061cdb36c826082bf0624af8dda3a2b4d4d1ee
maurirosas <mauriciorosasv@gmail.com>
2023-05-03 19:53:05 -0400

    Header Mejorado / Almacen

--- view/almacen.php
+++ view/almacen.php
@@ -1,41 +1,72 @@
 <?php require_once'../view/header.php';?>
-
-<div class="wrapper">
-            <div class="container-fluid">
-
                 <!-- Page-Title -->
                 <div class="row">
-                    <div class="col-sm-12">
-                        <div class="page-title-box">
-                            <div class="float-right hide-phone">
-                                <ul class="list-inline">
-                                    <li class="list-inline-item mr-3">
-                                        <input class="knob" data-width="48" data-height="48" data-linecap=round
-                                                           data-fgColor="#605daf" value="80" data-skin="tron" data-angleOffset="180"
-                                                           data-readOnly=true data-thickness=".1"/>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <span class="text-muted">Storage used</span>
-                                        <h6>400GB/524.84GB</h6>
-                                    </li>
-                                </ul>                                
-                            </div>
-                            <h4 class="page-title">Starter</h4>
-                            <div class="btn-group mt-2">
-                                <ol class="breadcrumb hide-phone p-0 m-0">
-                                    <li class="breadcrumb-item"><a href="#">Zoogler</a></li>
-                                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                                    <li class="breadcrumb-item active">Starter</li>
-                                </ol>
-                            </div>
-                            
+                    <div class="page-title-box">
+                        <div class="col-sm-6 col-md-3  mt-4">
+                                        <div class="text-center">
+                                            <!-- Small modal -->
+                                            <button type="button" class="btn btn-primary waves-effect waves-light" data-toggle="modal" data-animation="bounce" data-target=".bs-example-modal-center">Nuevo Almacen</button>
+                                        </div>
                         </div>
                     </div>
                 </div>
-                <!-- end page title end breadcrumb -->
+                
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
 
-            </div> <!-- end container -->
-        </div>
-        <!-- end wrapper -->
+                                <h4 class="mt-0 header-title">Example</h4>
+                                <p class="text-muted mb-4 font-13">This is an experimental awesome solution for responsive tables with complex data.</p>
 
+                                <div class="table-rep-plugin">
+                                    <div class="table-responsive b-0" data-pattern="priority-columns">
+                                        <table id="tech-companies-1" class="table table-striped mb-0">
+                                            <thead>
+                                            <tr>
+                                                <th>Nombre de Almacen</th>
+                                                <th data-priority="1">Direccion</th>
+                                                <th data-priority="3">Responsable</th>
+                                                <th data-priority="1">Capacidad</th>
+                                                <th data-priority="3">Telefono</th>
+                                                <th data-priority="3">Opcionnes</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <th>ALM-GRAL <span class="co-name">Almacen General</span></th>
+                                                <td>Central UCB</td>
+                                                <td>Lic. Pedro Perez</td>
+                                                <td>762/1000</td>
+                                                <td>6649792</td>
+                                                <td><button type="button" class="btn btn-primary waves-effect waves-light" data-toggle="modal" data-animation="bounce" data-target=".bs-example-modal-center">Ver Almacen</button></td>
+                                            </tr>
+                                            <tr>
+                                                <th>SUB-CTRL <span class="co-name">Sub Almacen - Central</span></th>
+                                                <td>Central UCB</td>
+                                                <td>Lic. Carolina Sanchez</td>
+                                                <td>430/500</td>
+                                                <td>6649798</td>
+                                                <td><button type="button" class="btn btn-primary waves-effect waves-light" data-toggle="modal" data-animation="bounce" data-target=".bs-example-modal-center">Ver Almacen</button></td>
+                                            </tr>
+                                            <tr>
+                                                <th>SUB-PSGRD <span class="co-name">Sub Almacen - Postgrado</span></th>
+                                                <td>Postgrado UCB</td>
+                                                <td>Lic. Mauricio Rosas</td>
+                                                <td>200/600</td>
+                                                <td>6658796</td>
+                                                <td><button type="button" class="btn btn-primary waves-effect waves-light" data-toggle="modal" data-animation="bounce" data-target=".bs-example-modal-center">Ver Almacen</button></td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div> <!-- end col -->
+                </div> <!-- end row --> 
 <?php require_once'../view/footer.php';?>
+<script type="text/javascript" src="../view/script/articulo.js"></script>
+
--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <title>Zoogler - Responsive Bootstrap 4 Admin Dashboard</title>
+        <title>Sistema de Almacenes UCB</title>
         <meta content="Admin Dashboard" name="description" />
         <meta content="Mannatthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -30,7 +30,7 @@
                     <!-- Logo container-->
                     <div class="logo">
                         <a href="index.html" class="logo">
-                            <i class="mdi mdi-bowling text-success mr-1"></i> <span class="hide-phone">Zoogler</span>
+                            <i class="mdi mdi-bowling text-success mr-1"></i> <span class="hide-phone">Sistema de Almacenes UCB</span>
                         </a>
 
                     </div>
