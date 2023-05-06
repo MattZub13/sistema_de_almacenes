6f66f62caba39af00210e04fd0b9138b6611b67b
maurirosas <mauriciorosasv@gmail.com>
2023-05-06 10:03:04 -0400

    Modelo Almacen

---
+++ model/Almacen.php
@@ -0,0 +1,23 @@
+<?php
+//Nos conectamos a la base de datos
+require_once "../config/Conexion.php";
+
+Class Almacen
+{
+    public function __construct()
+    {
+
+    }
+
+    public function listar()
+    {
+        $sql = "SELECT a.id_almacen, a.direccion_almacen, a.telefono, a.responsable, a.capacidad
+        FROM almacen a";
+        return ejecutarConsulta($sql);
+    }
+
+    public function insertar($nombre, $descripcion,$precio_unitario, $id_categoria)
+    {
+}
+
+?>
