a584287018da3d7f55a9f9043f1a88491ceda91c
anze28 <pedro2528anze@gmail.com>
2023-05-07 09:50:45 -0400

    navbar que si funcione digamoxs

--- view/header.php
+++ view/header.php
@@ -180,130 +180,57 @@
                         <ul class="navigation-menu">
 
                             <li class="has-submenu">
-                                <a href="index.html"><i class="dripicons-device-desktop"></i>Dashboard</a>
+                                <a href="./dashboard.php"><i class="dripicons-device-desktop"></i>Dashboard</a>
                             </li>
 
                             <li class="has-submenu">
-                                <a href="calender.html"><i class="dripicons-to-do"></i>Calendar</a>                                
-                            </li>
-
-                            <li class="has-submenu">
-                                <a href="#"><i class="dripicons-jewel"></i>User Interface</a>
+                                <a href="#"><i class="fa fa-boxes"></i>Almacen</a>
                                 <ul class="submenu megamenu">
                                     <li>
                                         <ul>
-                                            <li><a href="ui-alerts.html">Alerts</a></li>
-                                            <li><a href="ui-alertify.html">Alertify</a></li>
-                                            <li><a href="ui-badge.html">Badge</a></li>
-                                            <li><a href="ui-buttons.html">Buttons</a></li>
-                                            <li><a href="ui-carousel.html">Carousel</a></li>                                   
-                                            <li><a href="ui-cards.html">Cards</a></li>
-                                            <li><a href="ui-dropdowns.html">Dropdowns</a></li>        
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <ul>
-                                            <li><a href="ui-grid.html">Grid</a></li>
-                                            <li><a href="ui-images.html">Images</a></li>
-                                            <li><a href="ui-lightbox.html">Lightbox</a></li>
-                                            <li><a href="ui-modals.html">Modals</a></li>
-                                            <li><a href="ui-navs.html">Navs</a></li>
-                                            <li><a href="ui-progressbars.html">Progress Bars</a></li>
-                                            <li><a href="ui-pagination.html">Pagination</a></li>        
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <ul>
-                                            <li><a href="ui-popover-tooltips.html">Popover & Tooltips</a></li>
-                                            <li><a href="ui-rating.html">Rating</a></li>                                    
-                                            <li><a href="ui-rangeslider.html">Range Slider</a></li>
-                                            <li><a href="ui-sweet-alert.html">Sweet-Alert</a></li>                                    
-                                            <li><a href="ui-typography.html">Typography</a></li>
-                                            <li><a href="ui-tabs-accordions.html">Tabs & Accordions</a></li>
-                                            <li><a href="ui-video.html">Video</a></li> 
+                                            <li><a href="./almacen.php">Almacenes</a></li>
+                                            <li><a href="./articulo.php">Artículos</a></li>
+                                            <li><a href="./categoria.php">Categorias</a></li>     
                                         </ul>
                                     </li>
                                 </ul>
                             </li>
 
                             <li class="has-submenu">
-                                <a href="#"><i class="dripicons-blog"></i>Components</a>
-                                <ul class="submenu">
-                                    <li class="has-submenu">
-                                        <a href="#">Forms</a>
-                                        <ul class="submenu">
-                                            <li><a href="form-advanced.html">Form Advanced</a></li>
-                                            <li><a href="form-elements.html">Form Elements</a></li>
-                                            <li><a href="form-editors.html">Form Editors</a></li>
-                                            <li><a href="form-uploads.html">Form File Upload</a></li>
-                                            <li><a href="form-mask.html">Form Mask</a></li>
-                                            <li><a href="form-summernote.html">Summernote</a></li>
-                                            <li><a href="form-validation.html">Form Validation</a></li>                                    
-                                            <li><a href="form-xeditable.html">Form Xeditable</a></li>
-                                        </ul>
-                                    </li>
-                                    
-                                    <li class="has-submenu">
-                                        <a href="#">Charts</a>
-                                        <ul class="submenu">
-                                            <li><a href="charts-chartist.html">Chartist Chart</a></li>
-                                            <li><a href="charts-chartjs.html">Chartjs Chart</a></li>
-                                            <li><a href="charts-c3.html">C3 Chart</a></li>
-                                            <li><a href="charts-flot.html">Flot Chart</a></li>
-                                            <li><a href="charts-other.html">Jquery Knob Chart</a></li>
-                                            <li><a href="charts-morris.html">Morris Chart</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">Tables </a>
-                                        <ul class="submenu">
-                                            <li><a href="tables-basic.html">Basic Tables</a></li>
-                                            <li><a href="tables-datatable.html">Data Table</a></li>
-                                            <li><a href="tables-editable.html">Editable Table</a></li>
-                                            <li><a href="tables-responsive.html">Responsive Table</a></li> 
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">Icons</a>
-                                        <ul class="submenu">
-                                            <li><a href="icons-dripicons.html">Dripicons</a></li>
-                                            <li><a href="icons-fontawesome.html">Font Awesome</a></li>
-                                            <li><a href="icons-ion.html">Ion Icons</a></li>
-                                            <li><a href="icons-material.html">Material Design</a></li>
-                                            <li><a href="icons-themify.html">Themify Icons</a></li>
-                                            <li><a href="icons-typicons.html">Typicons Icons</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">Maps</a>
-                                        <ul class="submenu">
-                                            <li><a href="maps-google.html"> Google Map</a></li>
-                                            <li><a href="maps-vector.html"> Vector Map</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="has-submenu">
-                                <a href="#"><i class="dripicons-copy"></i>Pages</a>
+                                <a href="#"><i class="fa fa-car"></i>Vehículo</a>
                                 <ul class="submenu megamenu">
                                     <li>
                                         <ul>
-                                            <li><a href="pages-login.html">Login</a></li>
-                                            <li><a href="pages-register.html">Register</a></li>
-                                            <li><a href="pages-recoverpw.html">Recover Password</a></li>
-                                            <li><a href="pages-lock-screen.html">Lock Screen</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <ul>
-                                            <li><a href="pages-blank.html">Blank Page</a></li>
-                                            <li><a href="pages-404.html">Error 404</a></li>
-                                            <li><a href="pages-500.html">Error 500</a></li>
+                                            <li><a href="./vehiculo.php">Automoviles</a></li>
+                                            <li><a href="./surtidor.php">Surtidor</a></li>
                                         </ul>
                                     </li>
                                 </ul>
                             </li>
+
+                            <li class="has-submenu">
+                                <a href="#"><i class="dripicons-user-group"></i>Personal</a>
+                                <ul class="submenu">                                       
+
+                                    <li>
+                                        <ul>
+                                            <li><a href="./empleado.php">Empleados</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="has-submenu">
+                                        <a href="#">Oficina</a>
+                                        <ul class="submenu">
+                                            <li><a href="./oficina_dashboard.php">Oficinas</a></li>
+                                            <li><a href="./detalle_oficina.php">Detalles de Oficina</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="has-submenu">
+
+                            </li>
+
 
                         </ul>
                         <!-- End navigation menu -->
@@ -314,7 +241,7 @@
         <!-- End Navigation Bar-->
 
 
-        <div class="wrapper">
+        <!-- <div class="wrapper">
             <div class="container-fluid">
 
                 <!-- Page-Title -->
@@ -349,6 +276,6 @@
                 <!-- end page title end breadcrumb -->
 
             </div> <!-- end container -->
-        </div>
+        </div> -->
         <!-- end wrapper -->
             
